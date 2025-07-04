6236adffc57d8d53be77c4922e3321e51da3447b
Francisco <nomanfrancisco055@gmail.com>
2025-07-03 20:23:24 -0400

    feat: registro de modelos en admin

--- appointments/admin.py
+++ appointments/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
+from django import forms
+from .models import Patient, Doctor, Appointment
 
-# Register your models here.
+@admin.register(Patient)
+class PatientAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'email', 'phone_number', 'date_of_birth')
+    search_fields = ('first_name', 'last_name', 'email')
+    list_filter = ('date_of_birth',)
+
+@admin.register(Doctor)
+class DoctorAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'specialty', 'email', 'phone_number')
+    search_fields = ('first_name', 'last_name', 'specialty')
+    list_filter = ('specialty',)
+
+@admin.register(Appointment)
+class AppointmentAdmin(admin.ModelAdmin):
+    list_display = ('patient', 'doctor', 'start_time', 'end_time', 'status', 'is_past_appointment')
+    list_filter = ('status', 'doctor', 'start_time')
+    search_fields = ('patient__first_name', 'patient__last_name', 'doctor__first_name', 'doctor__last_name', 'notes')
+    date_hierarchy = 'start_time'
+    raw_id_fields = ('patient', 'doctor') # Mejora la UX para seleccionar FKs con muchos objetos
+
+    fieldsets = (
+        (None, {
+            'fields': ('patient', 'doctor', 'start_time', 'end_time', 'status')
+        }),
+        ('Información Adicional', {
+            'fields': ('notes',),
+            'classes': ('collapse',)
+        }),
+    )
